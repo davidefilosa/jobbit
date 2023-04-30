0c527b4f39a8648b036dd501a691e7c71bc0303e
Davide Filosa <davideclaypool@gmail.com!>
2023-04-30 10:18:00 +0200

    created Header and Welcome screen

--- components/home/welcome/Welcome.jsx
+++ components/home/welcome/Welcome.jsx
@@ -1,14 +1,60 @@
-import React from 'react'
-import { View, Text } from 'react-native'
+import { useState } from "react";
+import {
+  View,
+  Text,
+  TextInput,
+  TouchableOpacity,
+  Image,
+  FlatList,
+} from "react-native";
+import { useRouter } from "expo-router";
+import { icons, SIZES } from "../../../constants";
 
-import styles from './welcome.style'
+import styles from "./welcome.style";
 
 const Welcome = () => {
+  const router = useRouter();
+  const [search, setSearch] = useState("");
+  const jobTypes = ["Full Time", "Part Time", "Freelancer"];
+
   return (
     <View>
-      <Text>Welcome</Text>
-    </View>
-  )
-}
+      <View style={styles.container}>
+        <Text style={styles.userName}>Hello Davide</Text>
+        <Text style={styles.welcomeMessage}>Find your dream job!</Text>
+      </View>
+      <View style={styles.searchContainer}>
+        <View style={styles.searchWrapper}>
+          <TextInput
+            style={styles.searchInput}
+            value={search}
+            onChange={() => {}}
+            placeholder="What are your looking for?"
+          />
+        </View>
 
-export default Welcome
+        <TouchableOpacity style={styles.searchBtn} onPress={() => {}}>
+          <Image
+            source={icons.search}
+            resizeMode="contain"
+            style={styles.searchBtnImage}
+          />
+        </TouchableOpacity>
+      </View>
+      <View style={styles.tabsContainer}>
+        <FlatList
+          data={jobTypes}
+          renderItem={({ item }) => {
+            return (
+              <TouchableOpacity>
+                <Text>{item}</Text>
+              </TouchableOpacity>
+            );
+          }}
+        />
+      </View>
+    </View>
+  );
+};
+
+export default Welcome;
